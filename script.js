1be98066b19b025e57f82d921ce5b45faa99913a
Sidiney Oliveira <54914776+sidneyos@users.noreply.github.com>
2024-04-15 19:22:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function exemplo1() {
+ alert("Olá mundo");   
+}
+function exemplo2() {
+    let a = 5;
+    let b = 3;
+    alert(a + b);   
+   }
+function exemplo3() {
+    document.getElementById('id').style.backgroundColor = 'white';
+   }
+function exemplo4() {
+    document.getElementsByName('name')[0].style.backgroundColor = 'red';  
+   }
+function exemplo5() {
+    document.getElementsByClassName('class')[0].style.backgroundColor = 'green';  
+   }
